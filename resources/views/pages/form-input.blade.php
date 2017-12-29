ab327ec55089d6450ff09b7bdaa9855d78affcff
Huong <you@example.com>
2017-12-29 19:53:30 +0700

    forminput

--- resources/views/pages/form-input.blade.php
+++ resources/views/pages/form-input.blade.php
@@ -14,27 +14,33 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-6">
+            <h2>Sign Up</h2>
             <form>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Birthdate</label>
-                    <input type="text" class="form-control"  placeholder="dd/mm/yyyy">
+                    <input type="text" class="form-control" name="birthdate"  placeholder="dd/mm/yyyy">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Address</label>
-                    <input type="text" class="form-control"  placeholder="Enter address">
+                    <input type="text" class="form-control"  placeholder="Enter address" name="address">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Age</label>
-                    <input type="text" class="form-control"  placeholder="Enter your age">
+                    <input type="text" class="form-control" name="age"  placeholder="Enter your age">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Confirm Password</label>
+                    <input type="password" class="form-control" name="confirm_password" id="exampleInputPassword1" placeholder="Confirm Password">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
